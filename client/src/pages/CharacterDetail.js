7575fa0e438e3dd2d5b34559b3126d4f5f15d7d5
Vincent Revard <Vincent.Revard@gmail.com>
2024-04-15 20:40:58 -0700

    Add all Character fields to detail

--- client/src/pages/CharacterDetail.js
+++ client/src/pages/CharacterDetail.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const CharacterDetail = ({ id, name, description, alive, handleDeleteCharacter, startUpdate }) => {
+const CharacterDetail = ({ id, name, class_, race, alignment, age, alive, description, handleDeleteCharacter, startUpdate }) => {
     return (
         <li key={id}>
             <span>Name: {name}</span>
+            <span>Class: {class_}</span>
+            <span>Race: {race}</span>
+            <span>Alignment: {alignment}</span>
+            <span>Age: {age}</span>
+            <span>{`Is Alive: ${alive}`}</span>
             <span>Description: {description}</span>
-            <span>{`Is Active: ${alive}`}</span>
             <button onClick={startUpdate}>Update</button>
             <button onClick={() => handleDeleteCharacter(id)}>Delete</button>
         </li>
